f745b3329a7a1eb60ea5d26a82972bbf99ef6ecd
kuldeep <kuldeepdhakad152@gmail.com>
2024-04-24 22:04:29 +0530

    this is my first react api

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  let ABC=12;
+return (
+  <>
+    <h1>{ABC}</h1>
+    <h1>HELLO</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempora reprehenderit eos.</p>
+    <button>click</button>
+  </>
+)
+}
+
+export default App
